b07984772c3f8c24044321ce00ab7b96831fd511
Michela Frecchiami <michelafrecchiami@nebulab.it>
2021-08-18 13:56:37 +0200

    Style the cart badge of the top bar cart icon

--- app/overrides/solidus_demo/spree/base_helper.rb
+++ app/overrides/solidus_demo/spree/base_helper.rb
@@ -8,7 +8,7 @@ module SolidusDemo::Spree::BaseHelper
       css_class = 'empty'
     else
       text = "<div class='link-text'>#{current_order.item_count}</div>"\
-             "<span class='amount'>#{current_order.display_total.to_html}</span>"
+             #"<span class='amount'>#{current_order.display_total.to_html}</span>"
       css_class = 'full'
     end
     link_to text.html_safe, spree.cart_path, class: "cart-info #{css_class}"
